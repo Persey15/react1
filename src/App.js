93fc7171e0b218b74ee5d6ac01a9e27cddf15f26
Vlad <wlados@gmail.com>
2018-07-08 00:20:06 +0300

    Второй коммит

--- src/App.js
+++ src/App.js
@@ -2,35 +2,59 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import Grid from 'react-bootstrap/lib/Button';
-import Row from 'react-bootstrap/lib/Button';
-import Col from 'react-bootstrap/lib/Button';
+import Grid from 'react-bootstrap/lib/Grid';
+
+import styled from 'styled-components';
+
+import Header from './components/Header'
+import Menu from './components/Menu'
+import Main from './components/Main'
+
+const url = process.env.PUBLIC_URL + '/img/bg.jpg';
+const HeaderWrapper = styled.header`
+      width: 100%;
+      height: 49px;
+      background-color: #242424;
+      opacity: 0.8; 
+`
+const MenuWrapper = styled.div`
+        height: 89px;
+        padding-top: 20px;
+`
 
 
+const MainWrapper = styled.main`
+    height: 600px;
+    padding-top: 130px;
+    background: url(${url}) no-repeat;
+    background-size: cover;
+
+`
 class App extends Component {
   render() {
     return (
-         <div className= "wrapper">
-            <SayF name= "Vlad" surname ="Epaneshnikov" link="vk.com" /> 
-            <SayF name= "Katya" surname ="Rf" link="vk.com" /> 
-            <SayF name= "Ghost" surname ="tghb" link="vk.com" /> 
-    </div>
+         <div className= "App">
+            <HeaderWrapper>
+                <Grid>
+                  <Header />
+                 </Grid> 
+            </HeaderWrapper>   
+            <MenuWrapper>
+                <Grid>  
+                    <Menu />
+                </Grid>    
+            </MenuWrapper>
+            <MainWrapper>
+                <Grid>
+                    <Main />
+                </Grid>    
+            </MainWrapper>        
+        </div>
     );
   }
 }
 
 
-function SayF(props) {
-  return (
-    <div>
-    <h1> Моё имя {props.name}, фамилия - {props.surname}</h1>
-    <a href={props.link}> Ссылки на мой профиль </a>
-    </div>
-  )
-}
-
-
-const element = <SayF name= "Vlad" surname ="Epaneshnikov" link="vk.com" /> 
 
 
 export default App;
